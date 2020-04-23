a414d540ddc87315fe29caeafefb6864b4d5262a
sonnv <hailos1000@gmail.com>
2020-04-23 23:23:10 +0700

    update heroku

--- src/app/configs/route.configs.ts
+++ src/app/configs/route.configs.ts
@@ -1,5 +1,5 @@
-export default class RouteConfigs {
-    public static loginRoute = 'signin';
-    public static appRoute = '';
-    public static dashboardRoute = 'dashboard';
-}
+export const RouteConfigs = {
+    loginRoute: 'signin',
+    appRoute: '',
+    dashboardRoute: 'dashboard',
+};
