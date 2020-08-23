46a17814a6ec66ae7710eae15dffca3782ca9d94
sonnv <hailos1000@gmail.com>
2020-08-23 15:59:00 +0700

    update build

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit {
 
     constructor(
         private router: Router,
-        private auth: AuthService,
+        public auth: AuthService,
         private loggerService: LoggerServiceService,
         protected store: Store<AppStates>
     ) {
