f6631bd68ff32e457eeead9ffb1787f605dd3594
sonnv <hailos1000@gmail.com>
2020-09-19 18:53:12 +0700

    update

--- src/app/core/services/engine/allHouseEngine.ts
+++ src/app/core/services/engine/allHouseEngine.ts
@@ -1,7 +1,7 @@
 import {IBDSType} from '@core/services/engine/baseEngine';
 
 export class AllHouseEngine implements IBDSType {
-    private regex = /nguyên căn/muig;
+    private regex = /nguyên căn|cho thuê cả nhà|cả nhà/muig;
     validateType(entity: IBDSModel): boolean {
         return this.regex.test(entity.content);
     }
--- src/app/core/services/engine/findRoomEngine.ts
+++ src/app/core/services/engine/findRoomEngine.ts
@@ -1,7 +1,7 @@
 import {IBDSType} from '@core/services/engine/baseEngine';
 
 export class FindRoomEngine implements IBDSType {
-    private regex = /Cần tìm phòng|tìm phòng|muốn thuê phòng|tìm phòg|tìm nhà|cần thuê|Cần tìm/muig;
+    private regex = /Cần tìm phòng|tìm phòng|muốn thuê phòng|tìm phòg|tìm nhà|cần thuê|Cần tìm|tìm trọ/muig;
     private negativeRegex = /24\/7|24\/24/muig;
     validateType(entity: IBDSModel): boolean {
         return this.regex.test(entity.content) && !this.negativeRegex.test(entity.content);
