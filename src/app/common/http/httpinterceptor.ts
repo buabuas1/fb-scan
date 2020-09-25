92c9671bfd140dec7f29e241ec55dcd0e5083120
sonnv <hailos1000@gmail.com>
2020-09-25 10:38:36 +0700

    update auth

--- src/app/common/http/httpinterceptor.ts
+++ src/app/common/http/httpinterceptor.ts
@@ -1,15 +1,18 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import {SessionSettingsService} from '@core/services/settings';
 import {AuthService} from '@core/services/auth';
+import {Router} from '@angular/router';
+import {RouteConfigs} from '../../configs/route.configs';
 
 @Injectable()
 export class MyHttpInterceptor implements HttpInterceptor {
     constructor(private sessionSettingsService: SessionSettingsService,
-                private authService: AuthService
+                private authService: AuthService,
+                private router: Router
     ) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -35,6 +38,9 @@ export class MyHttpInterceptor implements HttpInterceptor {
                 // intercept the respons error and displace it to the console
                 console.log('Error Occurred');
                 console.log(error);
+                if (error.status === 401) {
+                    this.router.navigate([RouteConfigs.loginRoute]);
+                }
                 // return the error to the method that called it
                 return Observable.throw(error);
             }) as any;
