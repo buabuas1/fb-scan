3665e7a5a0e8e4d26ed606dac87a14362bbe5598
sonnv <sonnv@gmail.com>
2024-06-02 23:24:05 +0700

    chnage to render.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
     fbApi: 'https://graph.facebook.com/',
-    beHost: 'https://kind-ruby-bighorn-sheep-wig.cyclic.app/',
+    beHost: 'https://be-fb-scan-1.onrender.com/',
     toolKit: 'http://api.vltoolkit.com/'
 };
